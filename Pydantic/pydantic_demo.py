38cb36e7489b86e2d214ac73b3fc4141bbd7f911
Binod Tandan <binodtandan26@gmail.com>
2025-07-24 19:47:09 -0500

    Work with Annotation

--- Pydantic/pydantic_demo.py
+++ Pydantic/pydantic_demo.py
@@ -1,17 +1,22 @@
-from pydantic import BaseModel
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
-    name: str
-    age: int
-    weight: float
-    married: bool = False
-    allergies: Optional[List[str]] = None
+    name: Annotated[str, Field(max_length=200, title="Name of a patient", description="Name must be less than 200 characters", 
+                               example=['Binod','Rahul'])]
+    age: int = Field(gt=0)
+    email: EmailStr
+    linkedIn: AnyUrl
+    weight: Annotated[float, Field(gt=0, strict=True)]
+    married: Annotated[bool, Field(default=None, description="Is a patient married or Not")]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=3)]
     contact_details: Dict[str, str]
 
 def update_patient_name(patient: Patient):
     print(patient.name)
+    print(patient.email)
     print(patient.age)
+    print(patient.linkedIn)
     print(patient.weight)
     print(patient.married)
     print(patient.allergies)
@@ -19,7 +24,7 @@ def update_patient_name(patient: Patient):
     print('updated')
 
 # , 'allergies': ['pollen', 'dust']
-patient_info = {'name':"Binod", 'age': '20', 'weight': 143, 'married': True, 'contact_details': {'mobile': '12432222', 'email':'abc@gmail.com'}}
+patient_info = {'name':"Binod", 'age': 12, 'email': "abc@gmail.com",'linkedIn':'http://linkedin.com/123', 'weight': 143, 'allergies': ['pollen', 'dust', 'A'], 'married': True, 'contact_details': {'mobile': '12432222', 'email':'abc@gmail.com'}}
 patient1 = Patient(**patient_info)
 
 update_patient_name(patient1)
